35a54242f5ba01ada092b6d10c001297922612dd
https://github.com/sunnyrai87/custom_widget.git <sunnyraironey@gmail.com>
2023-11-06 23:14:23 +0530

    added splash screen

---
+++ lib/splash_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class splash extends StatefulWidget {
+
+
+  @override
+  State<splash> createState() => _State();
+}
+
+class _State extends State<splash> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
